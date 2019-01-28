8b04cbd0c54e67912b14430251251e88027d1a20
Pascal Precht <pascal.precht@gmail.com>
2019-01-28 15:13:44 +0100

    chore: add deploy script

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying website...\033[0m"
+
+npx hexo generate
+
+# delete old deploy branch
+git branch -D deploy
+
+git checkout -b deploy
+
+# Add changes to git.
+git add -f public
+
+# Commit changes.
+msg="chore(*): adding public folder `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+git subtree split -P public -b deploy-dist
+
+# Push source and build repos.
+git push -f origin deploy-dist:deploy-test
+git branch -D deploy-dist
+git checkout master
+git branch -D deploy
+
+
